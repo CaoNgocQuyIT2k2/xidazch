ccc40deae7bb0b481ed3645e0baea5f7e08a59e9
CaoNgocQuyIT2k2 <135012490+CaoNgocQuyIT2k2@users.noreply.github.com>
2026-05-02 20:59:00 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Mode from "./pages/Mode";
 import RoundGame from "./pages/RoundGame";
 import TotalGame from "./pages/TotalGame";
+import TienLenGame from "./pages/TienLenGame";
+
 
 export default function App() {
   return (
@@ -10,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Mode />} />
         <Route path="/round" element={<RoundGame />} />
         <Route path="/total" element={<TotalGame />} />
+  <Route path="/tienlen" element={<TienLenGame />} />
       </Routes>
     </BrowserRouter>
   );
